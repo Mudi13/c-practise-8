cc67c49c9cda6bab8e2d53abeb1b76250c2de02e
MUDIT CHAUHAN <93607579+Mudi13@users.noreply.github.com>
2024-11-15 23:54:02 +0530

    Add files via upload

---
+++ Left_Half_Pyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int rows;
+    scanf("%d",&rows);
+
+    for (int i = 1; i <= rows; i++)
+    {   
+        for (int j = i; j < rows; j++)
+        {
+                printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+
+}
