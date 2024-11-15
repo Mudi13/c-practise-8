154f72d451de5a7eb94be73214e19aed6e5382fd
MUDIT CHAUHAN <93607579+Mudi13@users.noreply.github.com>
2024-11-15 23:54:57 +0530

    Add files via upload

---
+++ Rhombus.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int rows;
+    scanf("%d",&rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = i; j < rows; j++)
+        {
+            printf(" ");
+        }
+    
+
+    for (int k = 1; k <= rows ; k++) 
+    {
+
+        printf("*");
+    }
+
+    printf("\n");
+
+    }
+}
